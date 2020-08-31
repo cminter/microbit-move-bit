196c024dbbd62f0f57cfb69550658eadcc0c40f7
Corey Minter <corey.minter@gmail.com>
2020-08-30 19:10:47 -0700

    refactor and remove blocks file

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
-x = 0
-y = 0
-coord: List[number] = []
 sideLength = 5
-direction = 0
-offset = 0
 def mapCoord(distance: number, offset: number, direction: number):
-    global x, y, coord
-    # east
-    if direction == 0: coord = [distance, offset]
-    # south
-    elif direction == 1:  coord = [4 - offset, distance]
-    # west
-    elif direction == 2:  coord = [4 - distance, 4 - offset]
-    # north
-    elif direction == 3:  coord = [offset, 4 - distance]
+    coord: List[number] = []
+    if direction == 0:  # east
+        coord = [distance, offset]
+    elif direction == 1:  # south
+        coord = [4 - offset, distance]
+    elif direction == 2:  # west
+        coord = [4 - distance, 4 - offset]
+    elif direction == 3:  # north
+        coord = [offset, 4 - distance]
     return coord
-def plotCoord(coord, brightness = 255):
-    led.plot_brightness(coord[0], coord[1], brightness)
 def shoot():
-    global direction, offset, sideLength
     direction = randint(0, 3)
     offset = randint(0, sideLength - 1)
-    for distance in range(sideLength + 2):
+    distance = 0
+    while distance <= sideLength + 2 - 1:
         plotCoord(mapCoord(distance - 2, offset, direction), 0)
         plotCoord(mapCoord(distance - 1, offset, direction), 50)
         plotCoord(mapCoord(distance, offset, direction), 200)
         basic.pause(100)
+        distance += 1
+def plotCoord(coord: List[number], brightness: number = 255):
+    led.plot_brightness(coord[0], coord[1], brightness)
 
 def on_forever():
     shoot()
--- main.ts
+++ main.ts
@@ -1,42 +1,38 @@
-let x = 0
-let y = 0
-let coord : number[] = []
 let sideLength = 5
-let direction = 0
-let offset = 0
 function mapCoord(distance: number, offset: number, direction: number): number[] {
-    
-    //  east
+    let coord : number[] = []
     if (direction == 0) {
+        //  east
         coord = [distance, offset]
     } else if (direction == 1) {
+        //  south
         coord = [4 - offset, distance]
     } else if (direction == 2) {
+        //  west
         coord = [4 - distance, 4 - offset]
     } else if (direction == 3) {
+        //  north
         coord = [offset, 4 - distance]
     }
     
-    //  south
-    //  west
-    //  north
     return coord
 }
 
-function plotCoord(coord: number[], brightness: number = 255) {
-    led.plotBrightness(coord[0], coord[1], brightness)
-}
-
 function shoot() {
-    
-    direction = randint(0, 3)
-    offset = randint(0, sideLength - 1)
-    for (let distance = 0; distance < sideLength + 2; distance++) {
+    let direction = randint(0, 3)
+    let offset = randint(0, sideLength - 1)
+    let distance = 0
+    while (distance <= sideLength + 2 - 1) {
         plotCoord(mapCoord(distance - 2, offset, direction), 0)
         plotCoord(mapCoord(distance - 1, offset, direction), 50)
         plotCoord(mapCoord(distance, offset, direction), 200)
         basic.pause(100)
+        distance += 1
     }
+}
+
+function plotCoord(coord: number[], brightness: number = 255) {
+    led.plotBrightness(coord[0], coord[1], brightness)
 }
 
 basic.forever(function on_forever() {
